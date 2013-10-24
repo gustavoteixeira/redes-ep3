b5fe8d5c5c7322af70fc33bc2e073fdce3bc6856
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-10-24 19:37:10 -0200

    Separa o comando em uma lista.

--- inputreader.py
+++ inputreader.py
@@ -1,7 +1,35 @@
+def find_first_of(s, occ, start = 0):
+    i = len(s) + 1
+    for c in occ:
+        f = s.find(c, start)
+        if f > -1 and f < i:
+            i = f
+    if i > len(s):
+        return -1
+    return i
+
+groupers = { '[': ']', '"': '"' }
+def oursplit(input):
+    resp = []
+    start = 0
+    while True:
+        i = find_first_of(input, groupers, start)
+        if i < 0:
+            resp.extend(input[start:].strip().split(' '))
+            break
+        else:
+            resp.extend(input[start:i].strip().split(' '))
+        e = input.index(groupers[input[i]], i+1)
+        resp.append(input[(i+1):e])
+        start = e + 1
+    return filter(None, resp)
+
 def parse(file):
     for line in file:
         line = line.strip()
         if not line or line[0] == '#':
             continue
-        print(line)
+        
+        command = oursplit(line)
+        print(command)
     #raise Exception("NYI")
