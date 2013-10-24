07dc2791694b42e5292054f84809c6eabbeb0b18
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-10-24 19:34:43 -0200

    Arrumando o strip.

--- inputreader.py
+++ inputreader.py
@@ -1,9 +1,7 @@
 def parse(file):
     for line in file:
-        line.strip()
-        if line:
-            if line[0] == '#' or line[0] == '\n':
-                continue
-            else:
-                print(line)
+        line = line.strip()
+        if not line or line[0] == '#':
+            continue
+        print(line)
     #raise Exception("NYI")
